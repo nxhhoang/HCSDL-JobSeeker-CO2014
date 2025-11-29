2d2bca9a32199e5eee2001cd8ebf9478c3af3346
quanghieu <ngo089120@gmail.com>
2025-11-29 17:09:44 +0700

    change init.sh add flag

--- database/scripts/init.sh
+++ database/scripts/init.sh
@@ -10,12 +10,20 @@ until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P 'admin@123' -Q "SELECT
   sleep 1
 done
 
-# Run all .sql scripts
-# Start from 00 to 04 ensure the order of execution
-for f in /scripts/*.sql; do
-  echo "Execute $f"
-  /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P 'admin@123' -i "$f" -C
-done
+
+if [ ! -f "/var/opt/mssql/data/.init_completed" ]; then
+    echo "Initializing database..."
+    # Run all .sql scripts
+    # Start from 00 to 04 ensure the order of execution
+    for f in /scripts/*.sql; do
+      echo "Execute $f"
+      /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P 'admin@123' -i "$f" -C
+    done
+    # Flag file to indicate initialization is done
+    touch /var/opt/mssql/data/.init_completed
+else
+    echo "Database already exists, skipping init"
+fi
 
 # Keep container running
 wait
